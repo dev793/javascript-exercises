0e02f025bb4955161c4bca25e0b21d735e8f338c
dev793 <117709900+dev793@users.noreply.github.com>
2025-12-27 15:58:03 +1300

    Completed javascript exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+    if (a < 0 || b < 0 ) return "ERROR";
 
+    let small = Math.min(a, b);
+    let large = Math.max(a, b);
+
+    let result = 0;
+
+    for (i = small; i <= large; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
